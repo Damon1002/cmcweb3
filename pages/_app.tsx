f917905fd6a0a92a8326286d66d91dc1b67f5ba3
Damon <damon.szh@gmail.com>
2022-12-03 20:18:40 +1100

    added trending sections which is the second part of the web

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log("damon");
+  // console.log(process.env.NEXT_PUBLIC_FULL_NAME);
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_APP_SERVER_URL ?? ""}
+      appId={process.env.NEXT_PUBLIC_APP_APPLICATION_ID ?? ""}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
